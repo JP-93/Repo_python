e3f05e7e6a5c3cdc36375fca58368923e52845e7
João Pedro Souza <62544169+JP-93@users.noreply.github.com>
2021-05-19 09:20:50 -0300

    Update log.py

--- log.py
+++ log.py
@@ -1,6 +1,6 @@
 import json
 
-with open('/Users/pedrobsouza/Documents/20210513T000013Z_20210513T000043Z_c9c50ef1.json') as f:
+with open('caminho do documento json') as f:
     #first_line = f.readline()
     for line in f:
         leitura = json.loads(line)
